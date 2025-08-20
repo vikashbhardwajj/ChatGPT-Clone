b40e111c15f24f381c22fdfc323d44a9e5e363bf
Vikash Bhardwaj <ind.vikashbhardwaj@gmail.com>
2025-08-20 20:22:51 +0530

    Add Pinecone dependency and implement vector service for memory management

---
+++ backend/src/services/vector.service.js
@@ -0,0 +1,26 @@
+// Import the Pinecone library
+const { Pinecone } = require("@pinecone-database/pinecone");
+
+// Initialize a Pinecone client with your API key
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+
+const cohortChatGptIndex = pc.Index("cohort-chat-gpt");
+
+const createMemory = async ({ vectors, metadata, messageId }) => {
+  await cohortChatGptIndex.upsert([
+    {
+      id: messageId,
+      values: vectors,
+      metadata,
+    },
+  ]);
+};
+
+const queryMemory = async ({ queryVector, limit = 5, metadata }) => {
+  const data = await cohortChatGptIndex.query({
+    vector: queryVector,
+    topK: limit,
+
+    filter: metadata ? { metadata } : undefined,
+  });
+};
